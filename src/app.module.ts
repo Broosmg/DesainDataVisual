0d668ce1d1e2925ff9bd3b248bd5619bd11153b1
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2023-05-23 21:34:39 +0700

    add hbs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from './typeorm.config';
 
 @Module({
   imports: [
@@ -10,7 +8,7 @@ import { config } from './typeorm.config';
       ignoreEnvFile: true,
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(config),
+    // TypeOrmModule.forRoot(config),
   ],
   controllers: [AppController],
 })
