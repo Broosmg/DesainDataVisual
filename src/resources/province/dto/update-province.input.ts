486fb6d805c2f3b2f3eb9661b33ee7119475d208
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2023-05-24 22:09:21 +0700

    add partial in update province

--- src/resources/province/dto/update-province.input.ts
+++ src/resources/province/dto/update-province.input.ts
@@ -1,9 +1,9 @@
-import { InputType, Field } from '@nestjs/graphql';
+import { InputType, Field, PartialType } from '@nestjs/graphql';
 import { PrimaryGeneratedColumn } from 'typeorm';
 import { CreateProvinceInput } from './create-province.input';
 
 @InputType()
-export class UpdateProvinceInput extends CreateProvinceInput {
+export class UpdateProvinceInput extends PartialType(CreateProvinceInput) {
   @Field()
   @PrimaryGeneratedColumn('uuid', { name: 'province_id' })
   provinceId: string;
