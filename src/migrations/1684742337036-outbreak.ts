afc89553a9aa6393bc35be6bc3eb84d0683e11f5
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2023-05-29 22:04:10 +0700

    fix dataset

--- src/migrations/1684742337036-outbreak.ts
+++ src/migrations/1684742337036-outbreak.ts
@@ -1,3 +1,6 @@
+import * as csv from 'csv-parser';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class Outbreak1684742337036 implements MigrationInterface {
@@ -49,9 +52,22 @@ export class Outbreak1684742337036 implements MigrationInterface {
     await queryRunner.query(
       'CREATE INDEX outbreak_idx ON outbreak (outbreak_category_id, district_id)',
     );
+    this.csvToDb(join(__dirname, '../../data/outbreak/dbd.csv'), queryRunner);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable('outbreak');
+  }
+
+  private csvToDb(path: string, queryRunner: QueryRunner) {
+    createReadStream(path)
+      .pipe(csv())
+      .on('data', async (row: any) => {
+        if (row) {
+          await queryRunner.query(
+            `INSERT INTO outbreak (outbreak_category_id, sufferer, dead, district_id, created_at) VALUES (${row.outbreak_category_id}, ${row.sufferer}, ${row.dead}, ${row.district_id}, '${row.created_at}')`,
+          );
+        }
+      });
   }
 }
