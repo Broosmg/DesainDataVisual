c350760fffb820489b2fcde724c4fdd7439dd8df
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2023-05-30 18:14:16 +0700

    delete data_type from env

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,10 +7,10 @@ import { join } from 'path';
 const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
-  type: configService.get<any>('DB_TYPE') || 'mysql',
+  type: 'postgres',
   host: configService.get<string>('DB_HOST') || 'localhost',
-  port: configService.get<number>('DB_PORT') || 3306,
-  username: configService.get<string>('DB_USERNAME') || 'root',
+  port: configService.get<number>('DB_PORT') || 5432,
+  username: configService.get<string>('DB_USERNAME') || 'postgre',
   password: configService.get<string>('DB_PASSWORD') || 'password',
   database: configService.get<string>('DB_NAME') || 'your_database_name',
   migrations: [join(__dirname, '../src/migrations/*.ts')],
