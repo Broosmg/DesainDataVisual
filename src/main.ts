559d4dbd0cce9b4179633196cecd47ecf1bdcd37
rafi237 <auliarafi473@gmail.com>
2023-10-21 15:57:17 +0700

    add public asset

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('hbs');
   await app.listen(new ConfigService().get('PORT') || 3000);
 }
