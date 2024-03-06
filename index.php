9fb94634e00daff37142f502a5ca00419ef91592
Aulia Rafi <auliarafi473@gmail.com>
2024-03-06 13:53:10 +0700

    add php module

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DVD (Desain Visualisasi Data)</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+        
+        #fullscreen-iframe {
+            width: 100%;
+            height: 100%;
+            border: none; /* Remove border */
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://outbreak-indicators.onrender.com" id="fullscreen-iframe"></iframe>    
+</body>
+</html>
